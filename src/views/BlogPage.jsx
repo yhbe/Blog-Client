cc3e916f5aa6be917e64f453aab64639946eed54
SHEF <Shefqetmush@gmail.com>
2023-03-12 21:06:31 -0400

    navigate from blogPage - home

--- src/views/BlogPage.jsx
+++ src/views/BlogPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import "./BlogPage.css";
 
 function BlogPage({ allBlogs, port, refreshPage }) {
   const { id } = useParams();
+  const navigate = useNavigate()
 
   const blog = allBlogs?.find(blog => blog._id === id) || null
 
@@ -46,32 +47,46 @@ function BlogPage({ allBlogs, port, refreshPage }) {
     } catch (error){
       console.log(error)
     }
+  }
 
+  const handleNavigation = () => {
+    navigate("/")
   }
 
   return (
-    <main className='blog-page-main-container'>
+    <main className="blog-page-main-container">
       {!blog && <h1>Blog not found</h1>}
       {blog && (
         <>
-        <div className="blog-container">
-        <p className="blog-title">{blog.title}</p>
-        <p className="blog-author">{blog.author}</p>
-        <p className="blog-body">{blog.body}</p>
-        <p className="blog-date">March 16th 2023</p>
-      </div>
-      <div className="blog-comment-section">
-        <div className="comments-container">
-        {commentJSX}
-        </div>
-      </div>
-        <form onSubmit={(event) => handleSubmit(event)} className='form-add-comment' action="">
-        <label htmlFor="addComment"></label>
-        <input className='form-add-comment-input'  type="text" name="addComment" id="addComment" placeholder='Add a comment'/>
-        <button type='submit' className='form-add-comment-button'>Add Comment</button>
-        </form>
+          <div className="blog-container">
+            <i onClick={() => handleNavigation()} className="fa-solid fa-arrow-left"></i>
+            <p className="blog-title">{blog.title}</p>
+            <p className="blog-author">{blog.author}</p>
+            <p className="blog-body">{blog.body}</p>
+            <p className="blog-date">March 16th 2023</p>
+          </div>
+          <div className="blog-comment-section">
+            <div className="comments-container">{commentJSX}</div>
+          </div>
+          <form
+            onSubmit={(event) => handleSubmit(event)}
+            className="form-add-comment"
+            action=""
+          >
+            <label htmlFor="addComment"></label>
+            <input
+              className="form-add-comment-input"
+              type="text"
+              name="addComment"
+              id="addComment"
+              placeholder="Add a comment"
+            />
+            <button type="submit" className="form-add-comment-button">
+              Add Comment
+            </button>
+          </form>
         </>
-      ) }
+      )}
     </main>
   );
 }
